f6203f96cb3f9072d2a7710dc55b6e74c5c362ed
Kir Proto Q <kir.proto@ya.ru>
2022-04-20 17:58:12 +0500

    Initial Commit

---
+++ Sochi/SochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SochiApp.swift
+//  Sochi
+//
+//  Created by KIR Q on 2022.04.20.
+//
+
+import SwiftUI
+
+@main
+struct SochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
